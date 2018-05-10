4cd320c7c065c8400458e6c82d4adf9c445349f0
Kaminetsky-asus <dev.ilya.kaminetsky@gmail.com>
2018-05-10 14:41:08 +0200

    Added_Main_Scene+Postition_Nail

---
+++ Assets/Rotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float xSpeed, ySpeed, zSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		  gameObject.transform.Rotate(Time.deltaTime*xSpeed, Time.deltaTime*ySpeed, Time.deltaTime*zSpeed, Space.World);
+
+	}
+}
